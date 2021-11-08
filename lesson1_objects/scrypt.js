d77f9f5c744c4ae882190b07dc7605a7a5391784
Rutigan <rutyganOfficial@gmail.com>
2021-11-08 22:11:48 +1000

    проверка

---
+++ lesson1_objects/scrypt.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const 
+      box = document.querySelectorAll('.box');
+ 
+
+  box.forEach( element => {
+    element.addEventListener('mouseover', (event) => {
+      event.target.style.transform = `scale(1.1)`;
+    })
+    element.addEventListener('mouseout', (event) => {
+      event.target.style.transform = `scale(1)`;
+    })
+  })
+  
+
+})
